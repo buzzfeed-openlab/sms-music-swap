7df0b8f07087319a8716b114c3cdda73c7240576
Cathy Deng <cathyd125@gmail.com>
2017-05-23 01:15:39 -0700

    fix env config

--- sms_swap/app_config.py
+++ sms_swap/app_config.py
@@ -25,4 +25,4 @@ else:
 
     TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID', '')
     TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN', '')
-    TWILIO_PHONE_NO = os.getenv('TWILIO_AUTH_TOKEN', '')
+    TWILIO_PHONE_NO = os.getenv('TWILIO_PHONE_NO', '')
