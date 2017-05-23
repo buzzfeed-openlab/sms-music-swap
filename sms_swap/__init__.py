5a9aa04ae7f074addae7776dfb6f0651579288a3
Cathy Deng <cathyd125@gmail.com>
2017-05-23 16:02:40 -0700

    stab

--- sms_swap/__init__.py
+++ sms_swap/__init__.py
@@ -5,6 +5,6 @@ from .app_config import DB_USER, DB_PW, DB_HOST, DB_NAME
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://{0}:{1}@{2}/{3}".format(DB_USER, DB_PW, DB_HOST, DB_NAME)
-    app.config['SQLALCHEMY_POOL_RECYCLE'] = 299
+    app.config['SQLALCHEMY_POOL_RECYCLE'] = 240
 
     return app
